3e10237f51cbbd0f663f9a32e14fdd71f3b84f77
Melih <75434646+CaptainMelih@users.noreply.github.com>
2026-08-02 10:32:43 +0300

    Fix React Rules of Hooks violation in Checkout.jsx and hydration mismatch in entry-client.jsx

--- src/entry-client.jsx
+++ src/entry-client.jsx
@@ -6,27 +6,7 @@ import './index.css'
 
 const container = document.getElementById('root')
 
-if (container && container.hasChildNodes()) {
-    try {
-        ReactDOM.hydrateRoot(
-            container,
-            <React.StrictMode>
-                <BrowserRouter>
-                    <App />
-                </BrowserRouter>
-            </React.StrictMode>
-        )
-    } catch (err) {
-        console.warn("Hydration failed, rendering via createRoot:", err)
-        ReactDOM.createRoot(container).render(
-            <React.StrictMode>
-                <BrowserRouter>
-                    <App />
-                </BrowserRouter>
-            </React.StrictMode>
-        )
-    }
-} else if (container) {
+if (container) {
     ReactDOM.createRoot(container).render(
         <React.StrictMode>
             <BrowserRouter>
